01625baa3d2dd0e3ad99d9ee2e0f6200c5f3c16b
Fill Evans <filipp.glinchuk@gmail.com>
2022-10-30 14:32:49 +0300

    did something

---
+++ test/module.py
@@ -0,0 +1,7 @@
+def greet(who):
+    print(f'Hello, {who}!')
+
+# if __name__ == '__main__':
+greet('Bob')
+greet('Ann')
+print(f"__name__ == '{__name__}'")
---
+++ test/test_module.py
@@ -0,0 +1,6 @@
+from module import greet
+
+
+print(f"__name__ == '{__name__}'")
+if __name__ == '__main__':
+    greet('Thomas')
